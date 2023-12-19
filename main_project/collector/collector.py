ec34e13dd3f36d682a00273494d8322933aba188
Orlando Nascimento <on_ferreira@id.uff.br>
2023-12-18 22:25:01 -0300

    Updating URL and fixing function names

--- main_project/collector/collector.py
+++ main_project/collector/collector.py
@@ -10,46 +10,46 @@ def run_communication_background(collector_dict):
     collector_instance._run_communication()
 
 
-URL_SYNTHESIS = "5095/synthesis"
+URL_SYNTHESIS = "http://dtsistemas:5095/synthesis"
 
 
 class Collector:
-    def __init__(self, synthesis_url=f"http://localhost:{URL_SYNTHESIS}/get_active_projects",
-                 communication_url=f"http://localhost:{URL_SYNTHESIS}/comunication_harverster_synthesis/",
+    def __init__(self, synthesis_url=f"{URL_SYNTHESIS}/get_active_projects",
+                 communication_url=f"{URL_SYNTHESIS}/comunication_collector_synthesis/",
                  project_list=None):
         self.synthesis_url = synthesis_url
         self.communication_url = communication_url
-        self.project_list = project_list or []
+        self.project_list = project_list or requests.get(self.synthesis_url).json()
 
     def _run_communication(self):
         logger.debug(f"Collector | _run_communication at {datetime.now()}")
         self._establish_connection_and_send_data()
 
     def _establish_connection_and_send_data(self):
-        logger.debug(f"Collector | _establish_connection_and_send_data at {datetime.now()}"
-                     f"\nActive projects: {self.project_list}")
+        logger.debug(f"Collector | _establish_connection_and_send_data at {datetime.now()}")
         try:
-            response = requests.get(self.synthesis_url)
+            response = requests.get(self.communication_url)
             projects = response.json()
-            harvested_data = self._get_harvested_data(projects)
-            self._send_harvested_data(harvested_data)
+            logger.debug(f"Collector | Projects Received {projects}")
+            collected_data = self._get_collected_data(projects)
+            self._send_collected_data(collected_data)
         except requests.exceptions.RequestException as e:
             logger.debug(f"Collector | Error establishing connection with synthesis: {e}")
 
-    def _get_harvested_data(self, projects):
-        harvested_data = {}
+    def _get_collected_data(self, projects):
+        collected_data = {}
         for project in projects:
             data_response = {}
             for tag in projects[project]["tags"]:
-                tag_response = requests.get(self.tags_urls[tag]).json()
+                tag_response = requests.get(projects[project]['link']).json()
                 data_response[tag] = tag_response
-            harvested_data[project] = data_response
-        return harvested_data
+            collected_data[project] = data_response
+        return collected_data
 
-    def _send_harvested_data(self, harvested_data):
+    def _send_collected_data(self, collected_data):
         headers = {'Content-Type': 'application/json'}
-        if harvested_data:
-            post_response = requests.post(self.communication_url, json=harvested_data, headers=headers)
+        if collected_data:
+            post_response = requests.post(self.communication_url, json=collected_data, headers=headers)
             logger.debug(f"Collector | Status Code: {post_response.status_code}")
         self.run_communication()
 
